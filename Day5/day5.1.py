3e97814907d429e135a3831975b825647d17b55f
natahlieb <natahliebeavers@gmail.com>
2017-12-05 21:31:18 -0500

    adding in day 5 solutions

---
+++ Day5/day5.1.py
@@ -0,0 +1,28 @@
+import sys
+
+inputList = []
+fp = open(sys.argv[1])
+
+#read in our file and construct the list
+for line in fp:
+	inputList.append(int(line))
+
+
+iterationCounter = 0
+currentIndex = 0
+
+#iterate until we're out of the loop -> need to see if go ahead of list (index <0) or outside of it (index > len(list))
+while(currentIndex < len(inputList) and currentIndex > -1 ):
+	#determine how far to jump
+	currentValue = inputList[currentIndex]
+
+	#increment our current location in the list by 1
+	inputList[currentIndex] += 1
+
+	#update the index we want to look at and increment our iteration counter
+	currentIndex += currentValue
+	iterationCounter += 1
+
+
+print('total iterations: {}'.format(iterationCounter))
+#print(inputList)
